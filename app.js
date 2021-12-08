f2acf3d1301c041972735b4c41f461b5dc81d870
Connor Hollis <connorhollis3d@gmail.com>
2021-12-07 20:42:33 -0700

    added required sections

--- app.js
+++ app.js
@@ -8,13 +8,29 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if(nameInput){
+                    return true
+                }else{
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
 
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username',
+            validate: gitName => {
+                if(gitName){
+                    return true
+                }else{
+                    console.log('Please enter your Github!');
+                    return false;
+                }
+            }
         },
 
         {
@@ -40,12 +56,28 @@ const promptProject = (portfolioData) => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project?',
+            validate: projectName => {
+                if(projectName){
+                    return true
+                }else{
+                    console.log('Please enter the name of your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: projectDescription => {
+                if(projectDescription){
+                    return true
+                }else{
+                    console.log('Please enter the description of your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -56,7 +88,15 @@ const promptProject = (portfolioData) => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: gitLink => {
+                if(gitLink){
+                    return true
+                }else{
+                    console.log('Please enter the Github link to your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
